d22f1cdfab432a9b8041efb7892bac1355ff8a83
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2024-08-30 04:33:05 -0400

    initial commit

---
+++ interpreter.go
@@ -0,0 +1,13 @@
+package main
+
+func compileEntries(entries []Entry) string {
+	return ""
+}
+
+// SDS stands for Simple Database Script
+
+func interpretSDS(text string) []Entry {
+	var out []Entry
+
+	return out
+}
